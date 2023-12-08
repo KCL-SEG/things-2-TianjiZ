f36801b4cd8b0af83120d1e1f63ccee4a2f91f33
Tianji Zhang <tianji.zhang@kcl.ac.uk>
2023-12-08 02:32:18 +0000

    Updated forms, template, and view for ThingForm

--- things/views.py
+++ things/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import ThingForm  # Import ThingForm from forms.py
 
 def home(request):
+    form = ThingForm()  # Create an instance of ThingForm
     return render(request, 'home.html', {'form': form})
